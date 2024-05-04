1882234fee415f3817ae9324b43db884425ab900
Capterlliar <401464788@qq.com>
2024-05-04 20:15:02 +0800

    clean code.

--- src/main/java/com/pdfTool/components/PDFViewer/event/Parameter.java
+++ src/main/java/com/pdfTool/components/PDFViewer/event/Parameter.java
@@ -4,5 +4,5 @@ package com.pdfTool.components.PDFViewer.event;
  * @author Patr1ick
  */
 public enum Parameter {
-    BEGIN, END, LEFT, PDF_LOADED, RENDER, RIGHT, THEME_CHANGED
+    BEGIN, END, LEFT, PDF_LOADED, RENDER, RIGHT
 }
